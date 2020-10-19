d780948417c74e7d13a2e812bd1ca4a497a23b99
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-10-19 17:12:16 +0200

    And that is enough for the day

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Jumbotron from "./components/jumbotron/index";
 
 function App() {
-  return <p>Welcome to Netflix (clone)!</p>;
+  return (
+    <div>
+      <p>Welcome to Netflix (clone)!</p>
+
+      <br />
+      <hr />
+      <br />
+
+      <Jumbotron></Jumbotron>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Jumbotron = ({}) => <p>I am the Jumbo!</p>;
+
+export default Jumbotron;
